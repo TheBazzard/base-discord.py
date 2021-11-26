875aae2c94dafac56b008ddf7ee3c1f170b156d0
Bazzard <90962705+TheBazzard@users.noreply.github.com>
2021-11-26 19:12:01 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "" # <--- Your token.
+
+bot = commmands.Bot(command_prefix = '') # <--- Your prefix.
+
+@bot.event                                    
+async def on_ready():                                
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(f"")) # Import your status in "" 
+    print('Bot is running.') # Message if bot start his work.
+
+bot.run(TOKEN) 
+
+# Be aware that this is the simplest base for a discord bot.
